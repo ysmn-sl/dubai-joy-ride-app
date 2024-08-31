685eacb7aebc2c919a28f788f8befd786333a4cc
yasaman <ysmn.dev@gmail.com>
2024-08-31 18:46:14 +0400

    update hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,10 @@ import Button from "./Button";
 const Hero: React.FC = () => {
   return (
     <section
-      className="bg-cover bg-center h-screen"
-      style={{ backgroundImage: 'url("/path/to/your/image.jpg")' }}
+      className="bg-cover bg-center h-[45vh]"
+      style={{ backgroundImage: 'url("/cover-hero.jpg")' }}
     >
-      <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
+      <div className="flex items-center justify-center h-full bg-black bg-opacity-40">
         <div className="text-center">
           <h1 className="text-4xl md:text-6xl text-white font-bold">
             Dubai Joy Ride
@@ -18,7 +18,7 @@ const Hero: React.FC = () => {
             to Dubai’s top theme parks and experience heart-pounding excitement
             and family fun!
           </p>
-          <Button variant="primary" className="mt-6">
+          <Button variant="primary" className="mt-8">
             Book Your Adventure
           </Button>
         </div>
