5547e00b1d805fdf70b98c091167db1177a83005
yasaman <ysmn.dev@gmail.com>
2024-09-15 19:46:16 +0400

    Add getAllThemeParkTicketById method to fetch tickets by park ID

--- src/lib/themParkService.ts
+++ src/lib/themParkService.ts
@@ -1,5 +1,5 @@
 import { ApiResponse } from "@/types/apiResponse";
-import { ThemePark } from "@/types/themPark";
+import { ThemePark, Ticket } from "@/types/themPark";
 import axios, { AxiosResponse } from "axios";
 
 const jsonFilePath = "/data/theme-parks.json";
@@ -27,6 +27,23 @@ export const getThemeParkById = async (
       response.data.find((themePark) => themePark.id === id) || null;
     return {
       data: findThemePark,
+      status: response.status,
+    };
+  } catch (error) {
+    console.error("Error fetching theme park by ID:", error);
+    throw error;
+  }
+};
+
+export const getAllThemeParkTicketById = async (
+  id: number
+): Promise<ApiResponse<Ticket[] | null>> => {
+  try {
+    const response: ApiResponse<ThemePark[]> = await getAllThemeParks();
+    const findThemePark =
+      response.data.find((themePark) => themePark.id === id) || null;
+    return {
+      data: findThemePark ? findThemePark.tickets : null,
       status: response.status,
     };
   } catch (error) {
