df27e3f4cd2b2e2660b88a14f9dffe6c0d0bf5a6
yasaman <ysmn.dev@gmail.com>
2024-09-20 20:19:17 +0400

    Add CartItemCart Component

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -4,11 +4,10 @@ import { useRouter } from "next/router";
 import { loadStripe } from "@stripe/stripe-js";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
 import { useCart } from "@/context/CartContext";
-import { ShoppingCartIcon } from "@heroicons/react/outline";
 import EmptyCart from "@/components/EmptyCart";
 import PaymentFailureModal from "@/components/PaymentFailureModal";
+import CartItemCard from "@/components/CartItemCard";
 
 // Load Stripe instance with your public key
 const stripePromise = loadStripe(
@@ -16,7 +15,7 @@ const stripePromise = loadStripe(
 );
 
 const CartPage: React.FC = () => {
-  const { cartItems, removeFromCart, updateCartItemQuantity } = useCart();
+  const { cartItems } = useCart();
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
@@ -24,10 +23,6 @@ const CartPage: React.FC = () => {
   const [showFailureModal, setShowFailureModal] = useState<Boolean>(false);
 
   const total = cartItems.reduce((acc, item) => acc + item.totalPrice, 0);
-
-  const handleQuantityChange = (itemId: number, quantity: number) => {
-    updateCartItemQuantity(itemId, quantity);
-  };
 
   const handleCheckout = async () => {
     setIsLoading(true);
@@ -53,7 +48,7 @@ const CartPage: React.FC = () => {
     if (canceled) {
       setShowFailureModal(true);
     }
-  }, [canceled, router]);
+  }, [canceled]);
 
   return (
     <>
@@ -66,93 +61,15 @@ const CartPage: React.FC = () => {
             My Basket ({cartItems.length} items)
           </p>
 
+          {/* Cart Items */}
           <div className="flex flex-col lg:flex-row px-4 py-2">
             <div className="flex-1 flex-col flex-grow">
               {cartItems.map((item) => (
-                <div
-                  key={item.id}
-                  className="relative flex w-full pb-4 mb-4 rounded-md shadow border border-gray-70"
-                >
-                  {/* Image Column */}
-                  <div className="flex-initial p-4">
-                    <Image
-                      src={item.product.images[0]}
-                      alt={item.product.name}
-                      width={128}
-                      height={90}
-                      className="w-[128px] h-[90px] object-cover rounded-sm md:w-[182px] md:h-[121px]"
-                    />
-                  </div>
-
-                  {/* Description Column */}
-                  <div className="flex-grow p-4 flex  md:flex-col relative">
-                    <div className="flex flex-col lg:flex-row justify-between items-start mb-2 md:mb-0">
-                      <div className="flex flex-col">
-                        <a
-                          href={`/parks/${item.product.id}`}
-                          className="text-lg font-semibold text-gray-800"
-                        >
-                          {item.product.name}
-                        </a>
-
-                        <div className="flex items-center text-gray-500 mt-2  text-sm lg:text-lg">
-                          <i className="fas fa-ticket mr-2"></i>
-                          <p> {item.ticket.type}</p>
-                        </div>
-
-                        <div className="flex items-center text-gray-500 mt-2 text-sm lg:text-lg">
-                          <i className="fas fa-calendar-alt mr-2"></i>
-                          <p>
-                            {item.date.day} {item.date.date}
-                          </p>
-                        </div>
-                      </div>
-
-                      {/* Total Amount */}
-                      <div className="flex items-center px-2 py-4">
-                        <div className="flex items-center border border-gray-200 rounded px-4 mr-2">
-                          <span className="text-gray-800 mr-1 text-sm lg:text-lg">
-                            Qty
-                          </span>
-                          <div className="relative">
-                            <select
-                              value={item.quantity}
-                              onChange={(e) =>
-                                handleQuantityChange(
-                                  item.id,
-                                  parseInt(e.target.value)
-                                )
-                              }
-                              className="appearance-none bg-transparent focus:outline-none pr-6 text-sm lg:text-lg"
-                            >
-                              {Array.from({ length: 4 }, (_, i) => (
-                                <option key={i + 1} value={i + 1}>
-                                  {i + 1}
-                                </option>
-                              ))}
-                            </select>
-                            <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-                              <i className="fas fa-chevron-down text-gray-700"></i>
-                            </div>
-                          </div>
-                        </div>
-
-                        <p className="text-sm lg:text-lg font-semibold text-gray-800 md:ml-4">
-                          AED {item.totalPrice}
-                        </p>
-                      </div>
-                    </div>
-                    <button
-                      className="absolute text-xl px-4 bottom-0 right-0 text-blue-600"
-                      onClick={() => removeFromCart(item.id)}
-                    >
-                      <i className="fa-solid fa-trash-can"></i>
-                    </button>
-                  </div>
-                </div>
+                <CartItemCard key={item.id} item={item} />
               ))}
             </div>
 
+            {/* Checkout section */}
             <div className="flex-non w-full lg:w-1/3 md:sticky md:top-4 lg:ml-6 md:ml-0 md:self-start">
               <div className="bg-gray-50 rounded-2xl px-8 py-4 shadow-lg border border-gray-50">
                 <p className="text-sm md:text-lg font-semibold text-gray-800 mb-2">
